426e0c4c6cb528e17fd3a3f491d529b912d8b4ea
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-10-25 20:00:13 -0300

    adding nav

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/cvcristian',
+      name: 'cvcristin',
+
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/cvjuan',
+      name: 'cvjuan'
+      
+    },
+    {
+      path: '/cvalexander',
+      name: 'cvalexander'
     }
   ]
 })
